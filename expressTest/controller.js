e2bf71ce853ebd98678c5073f8862beae6dc86cf
liuttweb <liuttweb@163.com>
2021-04-03 15:04:56 +0800

    feat: ssr模式快速常见网页

--- expressTest/controller.js
+++ expressTest/controller.js
@@ -1,15 +1,17 @@
 //用于处理数据，发送数据给前端
 
 const { template } = require('express-art-template')
-
-const list = (req, res) => {
+const fs = require('fs')
+const path = require('path')
+const list = (req, res) => { //服务端
     const str = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    const oDiv = template(__dirname + '/views/list', {
+    const oDiv = template(path.join(__dirname, '/views/list'), {
         data: str
     })
+    fs.writeFileSync(path.join(__dirname, './static/list.html'), oDiv)
     res.send(oDiv)
 }
-const nameData = (req, res) => {
+const nameData = (req, res) => { //js请求
     const data = ['name', 'age', 'litngotng', 20, 30];
     res.send(data)
 }
